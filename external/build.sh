bb9326ee0b94d785e93dc6f61c6d6fad66e1433b
xq <7416896+wx_f996a6a9fd@user.noreply.gitee.com>
2025-03-07 16:46:11 +0800

    tmp668

--- external/build.sh
+++ external/build.sh
@@ -103,10 +103,13 @@ popd
 
 # Move build lib into correct folders
 if [[ $RUNNER_OS == 'Windows' ]]; then
+    mkdir -p native/$NAME
     cp SDL/install_output/bin/SDL3.dll ../native/$NAME/SDL3.dll
 elif [[ $RUNNER_OS == 'Linux' ]]; then
+    mkdir -p native/$NAME
     cp SDL/install_output/lib/libSDL3.so ../native/$NAME/libSDL3.so
 elif [[ $RUNNER_OS == 'macOS' ]]; then
+    mkdir -p native/$NAME
     cp SDL/install_output/lib/libSDL3.dylib ../native/$NAME/libSDL3.dylib
 fi
 
